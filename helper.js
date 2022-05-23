a823ecb16a55d938e30195147ac6f914feff9865
KALIPPAN-SER02 <102555115+KALIPPAN-SER02@users.noreply.github.com>
2022-05-23 19:15:29 +0530

    Update helper.js

--- helper.js
+++ helper.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *aparna*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *aparna*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *aparna*:  ```" + msg + "```"
 }
 
 
